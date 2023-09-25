11caa2e3798d2a43343f5aaff0de7c52d14134b0
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-09-25 17:27:36 +0100

    Update bottom_nav.dart

--- lib/widgets/bottom_nav.dart
+++ lib/widgets/bottom_nav.dart
@@ -10,6 +10,9 @@ class BottomBar extends StatefulWidget {
 class _BottomBarState extends State<BottomBar> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        appBar: AppBar(
+      title: Text('My Tickets'),
+    ));
   }
 }
