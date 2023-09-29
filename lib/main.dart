1a889e4f891e8eb9360bd1c820c177e055b7851d
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-09-29 18:59:20 +0100

    Creating NavBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'home.dart';
+import 'widgets/bottom_nav.dart';
 
 void main() {
   runApp(const MyApp());
@@ -19,7 +20,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: Home(),
+      home: BottomBar(),
     );
   }
 }
--- lib/widgets/bottom_nav.dart
+++ lib/widgets/bottom_nav.dart
@@ -12,13 +12,14 @@ class _BottomBarState extends State<BottomBar> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('My Tickets'),
+        title: const Text('My Tickets'),
       ),
-      body: Center(
+      body: const Center(
         child: Text('My Body'),
       ),
-      bottomNavigationBar: BottomNavigationBar(items: [
-        BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home')
+      bottomNavigationBar: BottomNavigationBar(items: const [
+        BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+        BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
       ]),
     );
   }
