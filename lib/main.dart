58a91ca83470b33073a4a5219c4e105d0221f5d5
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-10-02 17:21:22 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 
-import 'screens/home.dart';
 import 'widgets/bottom_nav.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
