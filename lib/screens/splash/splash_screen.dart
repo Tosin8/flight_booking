3b2e9b04e84c624c31a813d1b03f75fc87d1d467
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-10-03 15:38:06 +0100

    Update splash_screen.dart

--- lib/screens/splash/splash_screen.dart
+++ lib/screens/splash/splash_screen.dart
@@ -10,6 +10,9 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        body: ListView(
+      children: [],
+    ));
   }
 }
