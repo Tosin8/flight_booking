c5d792e68441dc05026f366e8de1e2f0f8055db0
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-10-04 11:53:35 +0100

    Update login_body.dart

--- lib/screens/forms/login/login_body.dart
+++ lib/screens/forms/login/login_body.dart
@@ -11,22 +11,37 @@ class LoginBody extends StatefulWidget {
 class _LoginBodyState extends State<LoginBody> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Padding(
-        padding: const EdgeInsets.only(top: 120),
-        child: Column(
-          children: [
-            Image.asset("assets/logo-black-form.png"),
-            const Text(
-              'Kindly provide your login details below',
-              style: TextStyle(
-                  color: kSecondaryColor,
-                  fontSize: 15,
-                  fontWeight: FontWeight.w700),
-            )
-          ],
-        ),
+    return Stack(children: [
+      Container(
+        height: double.infinity,
+        width: double.infinity,
+        decoration: const BoxDecoration(),
       ),
-    );
+      Container(
+          height: double.infinity,
+          child: SingleChildScrollView(
+            physics: const AlwaysScrollableScrollPhysics(),
+            padding:
+                const EdgeInsets.symmetric(horizontal: 40.0, vertical: 40.0),
+            child:
+                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 120),
+                child: Column(
+                  children: [
+                    Image.asset("assets/logo-black-form.png"),
+                    const Text(
+                      'Kindly provide your login details below',
+                      style: TextStyle(
+                          color: kSecondaryColor,
+                          fontSize: 15,
+                          fontWeight: FontWeight.w700),
+                    ),
+                  ],
+                ),
+              ),
+            ]),
+          ))
+    ]);
   }
 }
