5d0b2fbd052960be1ebc5fa03734feb473695bc4
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-10-04 11:45:58 +0100

    Update login_body.dart

--- lib/screens/forms/login/login_body.dart
+++ lib/screens/forms/login/login_body.dart
@@ -11,19 +11,21 @@ class LoginBody extends StatefulWidget {
 class _LoginBodyState extends State<LoginBody> {
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 60),
-      child: Column(
-        children: [
-          Image.asset("assets/logo-black-form.png"),
-          const Text(
-            'Kindly provide your login details below',
-            style: TextStyle(
-                color: kSecondaryColor,
-                fontSize: 15,
-                fontWeight: FontWeight.w700),
-          )
-        ],
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.only(top: 120),
+        child: Column(
+          children: [
+            Image.asset("assets/logo-black-form.png"),
+            const Text(
+              'Kindly provide your login details below',
+              style: TextStyle(
+                  color: kSecondaryColor,
+                  fontSize: 15,
+                  fontWeight: FontWeight.w700),
+            )
+          ],
+        ),
       ),
     );
   }
